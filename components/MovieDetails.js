e5ba853dc30f77a82e81080fe615aef5435cf87a
jrivera17 <jrivera17@myseneca.ca>
2025-03-01 15:50:09 -0500

    Fixed the code

--- components/MovieDetails.js
+++ components/MovieDetails.js
@@ -4,17 +4,23 @@ export default function MovieDetails({ movie }) {
     return (
         <Container>
             <Row>
+                {/* Render poster if it exists */}
                 {movie.poster && (
                     <Col md={4}>
-                        <img src={movie.poster} alt={movie.title} className="img-fluid" />
+                        <img src={movie.poster} alt={movie.title} className="w-100" />
+                        <br /><br />
                     </Col>
                 )}
+
+                {/* Render movie details */}
                 <Col md={movie.poster ? 8 : 12}>
                     <p><strong>Directed By:</strong> {movie.directors?.join(', ') || 'N/A'}</p>
                     <p>{movie.fullplot || 'No plot available.'}</p>
                     <p><strong>Cast:</strong> {movie.cast?.length ? movie.cast.join(', ') : 'N/A'}</p>
                     <p><strong>Awards:</strong> {movie.awards?.text || 'N/A'}</p>
-                    <p><strong>IMDB Rating:</strong> {movie.imdb?.rating || 'N/A'} ({movie.imdb?.votes || 'N/A'} votes)</p>
+                    <p>
+                        <strong>IMDB Rating:</strong> {movie.imdb?.rating || 'N/A'} ({movie.imdb?.votes || 'N/A'} votes)
+                    </p>
                 </Col>
             </Row>
         </Container>
