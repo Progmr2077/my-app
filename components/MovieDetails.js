26f2a6f2c3549342e13b6c11b099df8816f9f6f7
jrivera17 <jrivera17@myseneca.ca>
2025-03-01 15:33:45 -0500

    fixed code

--- components/MovieDetails.js
+++ components/MovieDetails.js
@@ -1,3 +1,22 @@
-export default function MovieDetails() {
-    return <div>MovieDetails</div>;
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function MovieDetails({ movie }) {
+    return (
+        <Container>
+            <Row>
+                {movie.poster && (
+                    <Col md={4}>
+                        <img src={movie.poster} alt={movie.title} className="img-fluid" />
+                    </Col>
+                )}
+                <Col md={movie.poster ? 8 : 12}>
+                    <p><strong>Directed By:</strong> {movie.directors.join(', ')}</p>
+                    <p>{movie.fullplot}</p>
+                    <p><strong>Cast:</strong> {movie.cast?.length ? movie.cast.join(', ') : 'N/A'}</p>
+                    <p><strong>Awards:</strong> {movie.awards.text}</p>
+                    <p><strong>IMDB Rating:</strong> {movie.imdb.rating} ({movie.imdb.votes} votes)</p>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
