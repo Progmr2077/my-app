8b322ac2266eedb5b7f89672c064a9835112565e
jrivera17 <jrivera17@myseneca.ca>
2025-03-01 15:29:48 -0500

    fixed file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/styles/globals.css";
+import Layout from '@/components/Layout';
+import { SWRConfig } from 'swr';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (...args) => fetch(...args).then((res) => res.json()),
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
+  );
 }
+
+export default MyApp;
