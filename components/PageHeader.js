266418679f1c62dc8c46487b896fa74194edf852
jrivera17 <jrivera17@myseneca.ca>
2025-03-01 15:31:50 -0500

    fixed the code

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,3 +1,14 @@
-export default function PageHeader() {
-    return <div>PageHeader</div>;
+import { Card } from 'react-bootstrap';
+
+export default function PageHeader({ text }) {
+    return (
+        <>
+            <Card className="bg-light">
+                <Card.Body>
+                    <h1>{text}</h1>
+                </Card.Body>
+            </Card>
+            <br />
+        </>
+    );
 }
