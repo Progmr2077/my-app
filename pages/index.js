a715e800891c5dcdf57775d456d83c305bc0e7ac
jrivera17 <jrivera17@myseneca.ca>
2025-03-05 21:49:33 -0500

    fixed file

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 *
-* Name: Jacob Rivera Student ID: 109641233 Date: March 5th, 2025*
+* Name: Jacob Rivera Student ID: 109641233 Date: March 5th, 2025
 ********************************************************************************/
 import useSWR from 'swr';
 import { useState, useEffect, Suspense } from 'react';
